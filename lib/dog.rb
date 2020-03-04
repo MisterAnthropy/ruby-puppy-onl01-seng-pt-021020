6b82fe82d1718e1f998ebe2db95f7cbe1f56f60e
Tyler Croley <tjcroley@outlook.com>
2020-03-04 03:03:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,23 @@
-class Dog 
+class Dog
+
   attr_accessor :name
+
   @@all = []
+
   def initialize(name)
     @name = name
     @@all << self
-   end
-   def self.all
-     @@all.each do |dog|
-       puts dog.name
-     end
-   end
-   def self.print_all
-     @@all.clear
-   end
-     
- end
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
     
